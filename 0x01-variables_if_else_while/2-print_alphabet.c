eb8f9903771e4eb7b0ebd2e685e483ef879e45f6
nouha <nouha.abida@gmail.com>
2023-09-08 10:38:13 -0700

    olo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/* main - Prints the alphabetics
+/**
+ * main - Prints the alphabetics
  * Return: Always (success)
  */
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
